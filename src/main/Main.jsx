7b0209f91687af4ef283f3d850af91b971a65dad
V-Sugak <vicka.sugak2010@yandex.ru>
2022-02-24 21:27:53 +0300

    container, skills

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import style from "./Main.module.css"
+import style from "./Main.module.css";
+import styleContainer from "./../common/styles/Container.module.css"
 
 export const Main = () => {
     return <div className={style.mainBlock}>
-        <div className={style.text}>
-            <span>Hi There</span>
-            <h1>I am Viktoryia Sugak</h1>
-            <p>FRONT-END DEVELOPER</p>
+        <div className={styleContainer.container}>
+            <div className={style.text}>
+                <span>Hi There</span>
+                <h1>I am Sugak Viktoryia</h1>
+                <p>FRONT-END DEVELOPER</p>
+            </div>
+            <div className={style.photo}></div>
         </div>
-        <div className={style.photo}></div>
     </div>
 }
