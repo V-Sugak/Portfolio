7cfedc6b3902fe9bd7b876b3fd993b8e1da3b2d1
V-Sugak <vicka.sugak2010@yandex.ru>
2022-03-03 17:48:56 +0300

    skil

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import style from "./Skills.module.css"
 import styleContainer from "./../common/styles/Container.module.css"
+import {Skill} from "./skill/Skill";
 
 
 export const Skills = () => {
     return <div className={style.skillsBlock}>
         <div className={`${styleContainer.container}  ${style.skillsContainer}`}>
             <h2 className={style.title}>Skills</h2>
-            <div className={style.skills}></div>
+            <div className={style.skills}>
+                <Skill title={'JS'}
+                       description={'Programming language that conforms to the ECMAScript specification.'}/>
+                <Skill title={'Redux'} description={'JavaScript library for managing application state.'}/>
+                <Skill title={'React'}
+                       description={'JavaScript library for building user interfaces or UI components.'}/>
+            </div>
         </div>
     </div>
 }
